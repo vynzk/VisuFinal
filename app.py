52858227cbcf70d95a17553e5514a743755bdb9f
VALENTINA IGNACIA YÁÑEZ TORRES <85425661+vynzk@users.noreply.github.com>
2024-12-09 18:51:39 -0300

    inicial funciona
    Co-authored-by: alfajoruwu <alfajoruwu@users.noreply.github.com>

--- app.py
+++ app.py
@@ -5,19 +5,18 @@ from dash.dependencies import Input, Output
 
 data = pd.read_excel('Llamados2024.xlsx')
 
+# Figuras ############################################
+
 fig = px.scatter(data, x='prevision', y='estado')
+
+######################################################
 
 app = Dash(__name__)
 app.layout = html.Div([
     html.H1("Mi Proyecto de Visualización"),
-    # dcc.Graph(figure=fig),
-    # dcc.Slider(min=0, max=100, step=1, value=50)
+    dcc.Graph(figure=fig),
+    dcc.Slider(min=0, max=100, step=1, value=50)
 ])
 
-# @app.callback(
-#     Output('graph-id', 'figure'),
-#     Input('slider-id', 'value')
-# )
-# def update_graph(slider_value):
-#     filtered_data = data[data['value'] < slider_value]
-#     return px.scatter(filtered_data, x='x_c', y='y_column')
+if __name__ == '__main__':
+    app.run_server(debug=True)
